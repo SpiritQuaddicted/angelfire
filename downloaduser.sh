932ce24f6b756d563a08f90617ed28a9b4a2980c
SpiritQuaddicted <spirit@quaddicted.com>
2015-05-08 22:10:30 +0200

    changes

--- downloaduser.sh
+++ downloaduser.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -e
-set -u
+#set -e # will fail somehow
+#set -u # will fail somehow
+#set -o pipefail # will fail somehow
 
-# FEED ME USERS LIKE THIS
-# bash downloaduser.sh realm/name
-
-workingdirectory=/tmp/
+workingdirectory=/tmp/angelfire/
 cd "${workingdirectory}"
 
 date=$(date +%Y%m%d)
@@ -22,18 +20,25 @@ mkdir -p "${prettyuser}" && cd "${prettyuser}"
 wget -nv -x --no-cookies --directory-prefix="${prettyuser}_${date}" http://www.angelfire.com/"${user}"/sitemap.xml
 
 # extract all the urls from it
-grep -Eo '<loc>.*</loc>' "${prettyuser}_${date}"/www.angelfire.com/"${user}"/sitemap.xml | sed 's#<loc>##' | sed 's#</loc>##' > "${prettyuser}_urls"
-wc -l "${prettyuser}_urls"
+grep -Eo '<loc>.*</loc>' "${prettyuser}_${date}"/www.angelfire.com/"${user}"/sitemap.xml | sed 's#<loc>##' | sed 's#</loc>##' > "${prettyuser}.urls"
+wc -l "${prettyuser}.urls"
 
-# feed the urls to wget -m
-# TODO are angelfire.com and lycos.com enough?
-wget -m --no-parent --no-cookies -e robots=off --page-requisites \
---adjust-extension --convert-links \
---append-output="${prettyuser}_${date}.log" \
---directory-prefix="${prettyuser}_${date}" \
---warc-file="${prettyuser}_${date}" \
---domains=angelfire.com,lycos.com \
---reject-regex='(www.angelfire.com\/adm\/ad\/|www.angelfire.com\/doc\/images\/track\/ot_noscript\.gif)' \
--i "${prettyuser}_urls"
-
-echo "${prettyuser} done"
+if grep -Eq "/index.blog$" ${prettyuser}.urls
+then
+	echo "INFINITE CALENDAR DETECTED, ABANDON SHIP (skipping this user, see ${workingdirectory}blogusers.todo)"
+	echo "${prettyuser}" >> "${workingdirectory}"blogusers.todo
+else
+	# feed the urls to wget -m
+	# TODO are angelfire.com and lycos.com enough?
+	wget -m --no-parent --no-cookies -e robots=off --page-requisites \
+	--adjust-extension --convert-links \
+	--append-output="${prettyuser}_${date}.log" \
+	--directory-prefix="${prettyuser}_${date}" \
+	--warc-file="${prettyuser}_${date}" \
+	--warc-cdx \
+	--domains=angelfire.com,lycos.com \
+	--reject-regex='(www.angelfire.com\/adm\/ad\/|www.angelfire.com\/doc\/images\/track\/ot_noscript\.gif)' \
+	-i "${prettyuser}.urls"
+	
+	echo "${prettyuser} done"
+fi
